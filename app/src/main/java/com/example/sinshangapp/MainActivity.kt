a062bf119743395c16c040e9e07d1eadb0cc40e8
prakash <apuiirom@gmailcom>
2024-10-10 16:08:17 +0530

    First Commit

---
+++ app/src/main/java/com/example/sinshangapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.sinshangapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.sinshangapp.ui.theme.SinshangAppTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SinshangAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                }
+            }
+        }
+    }
+}
